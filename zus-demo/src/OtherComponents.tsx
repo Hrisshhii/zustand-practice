fd3d7606679f37106c64b1e1d6bc100a9c5d9a55
Hrishi <rathodhrishi12345@gmail.com>
2026-05-05 01:08:57 +0530

    intendation fixes

--- zus-demo/src/OtherComponents.tsx
+++ zus-demo/src/OtherComponents.tsx
@@ -1,23 +1,17 @@
 import { useCounter } from "./store"
 
-const OtherComponents = () => {
+const OtherComponents=()=>{
   const {increment}=useCounter();
   const {decrement}=useCounter();
 
   return (
     <div className="flex gap-4 justify-center">
-      <button
-        onClick={decrement}
-        className="px-5 py-2 rounded-xl bg-red-500 text-white"
-      >
-        −
+      <button onClick={decrement} className="px-5 py-2 rounded-xl bg-red-500 text-white" >
+        {`-`}
       </button>
 
-      <button
-        onClick={increment}
-        className="px-5 py-2 rounded-xl bg-green-500 text-white"
-      >
-        +
+      <button onClick={increment} className="px-5 py-2 rounded-xl bg-green-500 text-white" >
+        {`+`}
       </button>
     </div>
   )
