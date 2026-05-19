27cf569509b68ea6bfc2b780c8c9d81d329e7842
Hrishi <rathodhrishi12345@gmail.com>
2026-05-19 23:19:04 +0530

    Intendation fixes

--- zus-demo/src/App.tsx
+++ zus-demo/src/App.tsx
@@ -4,21 +4,16 @@ import { useCounter } from "./store"
 function App() {
   const {count}=useCounter();
   //const count=useCounter(state=>state.count);
-
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="bg-gray-900/70 rounded-2xl p-10 text-center">
-        
         <h1 className="text-5xl font-extrabold text-white mb-6">
           Counter
         </h1>
-
         <p className="text-3xl font-semibold text-blue-400 mb-8">
           {count}
         </p>
-
         <OtherComponents />
-
       </div>
     </div>
   )
