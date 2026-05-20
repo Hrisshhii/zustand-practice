9d390ae427753dcfc73f80c632f2331e99546f73
Hrishi <rathodhrishi12345@gmail.com>
2026-05-20 22:58:06 +0530

    Typo fix

--- zus-demo/src/store.ts
+++ zus-demo/src/store.ts
@@ -8,6 +8,6 @@ type CounterStore={
 
 export const useCounter=create<CounterStore>((set)=>({
   count:0,
-  increment:()=>set((state)=>({count: state.count + 1})),
+  increment:()=>set((state)=>({count: state.count+1})),
   decrement:()=>set(state=>({count: state.count-1})),
 }));
