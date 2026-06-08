2d6bf343ba5d6b57f57424b39480c5c017f8d6ea
Hrishi <rathodhrishi12345@gmail.com>
2026-06-09 00:03:12 +0530

    intendation fixes

--- zus-demo/src/OtherComponents.tsx
+++ zus-demo/src/OtherComponents.tsx
@@ -1,8 +1,8 @@
 import { useCounter } from "./store"
 
-const OtherComponents=()=>{
-  const {increment}=useCounter();
-  const {decrement}=useCounter();
+const OtherComponents = () => {
+  const {increment} = useCounter();
+  const {decrement} = useCounter();
 
   return(
     <div className="flex gap-4 justify-center">
