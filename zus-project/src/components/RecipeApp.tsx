53310fa929395a10d576637f93be50b3d3e5d07e
Hrishi <rathodhrishi12345@gmail.com>
2026-06-16 22:53:29 +0530

    Creating the state to be used

--- zus-project/src/components/RecipeApp.tsx
+++ zus-project/src/components/RecipeApp.tsx
@@ -1,7 +1,13 @@
 import useStore from "../store/useStore";
+import { useState } from "react";
 
 const RecipeApp=()=>{
   const {recipes,addRecipe,removeRecipe}=useStore();
+  const [name,setName]=useState<string>('');
+  const [ingredients,setIngredients]=useState<string>('');
+  const [instructions,setInstructions]=useState<string>('');
+  const [editing,setEditing]=useState(null);
+
   return (
     <div>RecipeApp</div>
   )
