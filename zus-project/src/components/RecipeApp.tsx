7ef61af71325825d698ff3e0b1884c7f6fae992b
Hrishi <rathodhrishi12345@gmail.com>
2026-06-16 22:55:36 +0530

    Creating interface for editing the recipe

--- zus-project/src/components/RecipeApp.tsx
+++ zus-project/src/components/RecipeApp.tsx
@@ -1,15 +1,22 @@
 import useStore from "../store/useStore";
 import { useState } from "react";
 
+interface Recipe{
+  id:number;
+  name:string,
+  ingredients:string[];
+  instructions:string[];
+}
+
 const RecipeApp=()=>{
   const {recipes,addRecipe,removeRecipe}=useStore();
   const [name,setName]=useState<string>('');
   const [ingredients,setIngredients]=useState<string>('');
   const [instructions,setInstructions]=useState<string>('');
-  const [editing,setEditing]=useState(null);
+  const [editing,setEditing]=useState<Recipe | null>(null);
 
   return (
-    <div>RecipeApp</div>
+    <div className=""></div>
   )
 }
 
