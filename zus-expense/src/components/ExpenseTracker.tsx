3933170cd0f4eca0789afa0f0f3e9b44b98a7960
Hrishi <rathodhrishi12345@gmail.com>
2026-06-20 21:40:03 +0530

    Fixing typo

--- zus-expense/src/components/ExpenseTracker.tsx
+++ zus-expense/src/components/ExpenseTracker.tsx
@@ -18,7 +18,7 @@ const ExpenseTracker=()=>{
   };
 
   return(
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-400 to-blue-500">
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-purple-400 to-blue-500">
       <div className="bg-white p-8 rounded-lg shadow-xl w-full max-w-lg">
         <h1 className="text-4xl font-extrabold mb-6 text-center text-purple-500">Expense Tracker</h1>
 
