701adaf519093c1f07d6d98fe5296d96f280c3b2
Hrishi <rathodhrishi12345@gmail.com>
2026-06-21 02:37:45 +0530

    impprting usePasswordStore from store

--- zus-password/src/components/PasswordGenerator.tsx
+++ zus-password/src/components/PasswordGenerator.tsx
@@ -1,7 +1,14 @@
+import usePasswordStore from "../store";
+
 
 const PasswordGenerator = () => {
+  const {generatedPassword}=usePasswordStore()
+
   return (
-    <div>PasswordGenerator</div>
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-purple-400 to-blue-500">
+      <h1>Password Generator</h1>
+      <p>{generatedPassword}</p>
+    </div>
   )
 }
 
