e204b70fdfdf2659746e42f3a41625c082a2330d
Hrishi <rathodhrishi12345@gmail.com>
2026-06-23 23:32:41 +0530

    Refactor layout of PasswordGenerator component for improved styling and structure

--- zus-password/src/components/PasswordGenerator.tsx
+++ zus-password/src/components/PasswordGenerator.tsx
@@ -4,13 +4,19 @@ const PasswordGenerator=()=>{
   const {length,setLength,includeUppercase,includeLowercase,includeNumbers,includeSymbols,toggleUppercase,toggleLowercase,toggleNumbers,toggleSymbols,generatedPassword}=usePasswordStore()
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-linear-to-r from-purple-400 to-blue-500">
-      <h1 className="text-3xl font-bold text-white">Password Generator</h1>
-      <div className="flex flex-col gap-4">
-        <label htmlFor="length" className="block text-sm font-medium text-gray-700">Password Length</label>
-        <input type="number" id="length" value={length} onChange={e=>setLength(+e.target.value)} min={4} max={64}
-          className="mt-1 block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
-        />
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-purple-400 to-blue-500">
+      <div className="bg-white p-8 rounded-lg shadow-xl w-full max-w-lg">
+        <h1 className="text-3xl font-bold">Password Generator</h1>
+        <div className="flex flex-col gap-4">
+          <label htmlFor="length" className="block text-sm font-medium text-gray-700">Password Length</label>
+          <input type="number" id="length" value={length} onChange={e=>setLength(+e.target.value)} min={4} max={64}
+            className="mt-1 block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+          />
+        </div>
+        <div className="flex items-center">
+          <input type="checkbox" checked={includeNumbers} onChange={toggleNumbers}/>
+          <label className="ml-2 text-sm">Include Numbers</label>
+        </div>
       </div>
     </div>
   )
