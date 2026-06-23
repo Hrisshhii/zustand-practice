3d1807c33dc9938a04a9032451a8463bcf7c10f9
Hrishi <rathodhrishi12345@gmail.com>
2026-06-23 23:21:02 +0530

    Creating structure

--- zus-password/src/components/PasswordGenerator.tsx
+++ zus-password/src/components/PasswordGenerator.tsx
@@ -4,9 +4,11 @@ const PasswordGenerator=()=>{
   const {length,setLength,includeUppercase,includeLowercase,includeNumbers,includeSymbols,toggleUppercase,toggleLowercase,toggleNumbers,toggleSymbols,generatedPassword}=usePasswordStore()
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-purple-400 to-blue-500">
-      <h1>Password Generator</h1>
-      <p>{generatedPassword}</p>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-linear-to-r from-purple-400 to-blue-500">
+      <h1 className="text-3xl font-bold text-white">Password Generator</h1>
+      <div className="flex flex-col gap-4">
+        <label htmlFor="length" className="block text-sm font-medium text-gray-700">Password Length</label>
+      </div>
     </div>
   )
 }
