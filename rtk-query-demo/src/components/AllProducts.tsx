67c02c973969930c005afb14273145cbfc030516
Hrishi <rathodhrishi12345@gmail.com>
2026-06-28 21:36:44 +0530

    Implement product listing in AllProducts component with loading and error handling

--- rtk-query-demo/src/components/AllProducts.tsx
+++ rtk-query-demo/src/components/AllProducts.tsx
@@ -1,7 +1,22 @@
+import { useGetAllProductsQuery } from "../app/service/dummyData";
 
 const AllProducts = () => {
+  const {data,isError,isLoading}=useGetAllProductsQuery("");
+
+  if(isError){
+    return <h1>Error</h1>
+  }
+
+  if(isLoading){
+    return <h1>Loading</h1>
+  }
+
   return (
-    <div>AllProducts</div>
+    <div>
+      {data?.products.map((p:any)=>(
+        <h1 key={p.id}>{p.title}</h1>
+      ))}
+    </div>
   )
 }
 
