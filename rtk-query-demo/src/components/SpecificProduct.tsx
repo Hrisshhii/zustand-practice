afc3334f43fac44d345a1818b5b0ecd9ccad6be5
Hrishi <rathodhrishi12345@gmail.com>
2026-06-29 22:03:40 +0530

    Using the hook to get specific data

--- rtk-query-demo/src/components/SpecificProduct.tsx
+++ rtk-query-demo/src/components/SpecificProduct.tsx
@@ -1,7 +1,23 @@
+import {useGetProductByIdQuery} from '../app/service/dummyData';
 
 const SpecificProduct = () => {
+  const {data,isLoading,isError}=useGetProductByIdQuery(2);
+  console.log(data);
+
+  if(isError){
+    return <h1>Error</h1>
+  }
+
+  if(isLoading){
+    return <h1>Loading</h1>
+  }
+
   return (
-    <div>SpecificProduct</div>
+    <div>
+      <p>{data.id} : {data.title}</p>
+      <p>{data.description}</p>
+      <img src={data.images}/>
+    </div>
   )
 }
 
