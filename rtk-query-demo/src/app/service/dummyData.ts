8409ccfa5d66e850bea3e7b8509b05105fb492c0
Hrishi <rathodhrishi12345@gmail.com>
2026-06-29 21:57:10 +0530

    Creating another endpoint for a sepecific id search

--- rtk-query-demo/src/app/service/dummyData.ts
+++ rtk-query-demo/src/app/service/dummyData.ts
@@ -7,7 +7,10 @@ export const productApi=createApi({
     getAllProducts:builder.query({
       query:()=>'/products'
     }),
+    getProductById:builder.query({
+      query:(id)=>`/products/${id}`
+    })
   }),
 })
 
-export const {useGetAllProductsQuery}=productApi
+export const {useGetAllProductsQuery,useGetProductByIdQuery}=productApi
