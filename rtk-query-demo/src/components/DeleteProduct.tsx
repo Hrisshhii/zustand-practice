6a14b364409f7bbcd2d1e58d9c19f4017710fb02
Hrishi <rathodhrishi12345@gmail.com>
2026-07-03 21:49:05 +0530

    Importing and extracting form the mutation endpoint

--- rtk-query-demo/src/components/DeleteProduct.tsx
+++ rtk-query-demo/src/components/DeleteProduct.tsx
@@ -1,5 +1,15 @@
+import {useDeleteProductMutation} from "../app/service/dummyData";
 
 const DeleteProduct = () => {
+  const [deleteProduct,{data,isError,isLoading}]=useDeleteProductMutation();
+
+  if(isError){
+    return <h1>Error</h1>
+  }
+
+  if(isLoading){
+    return <h1>Loading</h1>
+  }
   return (
     <div>DeleteProduct</div>
   )
