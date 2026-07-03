1cb3e13469bb8ae433757c92e5d83ab06a228416
Hrishi <rathodhrishi12345@gmail.com>
2026-07-03 21:44:40 +0530

    registering component

--- rtk-query-demo/src/App.tsx
+++ rtk-query-demo/src/App.tsx
@@ -1,8 +1,9 @@
 // import AllProducts from "./components/AllProducts"
 // import SpecificProduct from "./components/SpecificProduct"
 // import AddNewProduct from "./components/AddNewProduct"
+// import UpdateProduct from "./components/UpdateProduct"
 
-import UpdateProduct from "./components/UpdateProduct"
+import DeleteProduct from "./components/DeleteProduct"
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       {/* <AllProducts/> */}
       {/* <SpecificProduct/> */}
       {/* <AddNewProduct/> */}
-      <UpdateProduct productId={4}/>
+      {/* <UpdateProduct productId={4}/> */}
+      <DeleteProduct/>
 
     </div>
   )
