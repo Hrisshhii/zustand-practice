bb5a6e9d8dd73109bf5510319fbba0fc4c3af468
Hrishi <rathodhrishi12345@gmail.com>
2026-07-03 21:51:46 +0530

    Creating the deletion function

--- rtk-query-demo/src/components/DeleteProduct.tsx
+++ rtk-query-demo/src/components/DeleteProduct.tsx
@@ -1,6 +1,6 @@
 import {useDeleteProductMutation} from "../app/service/dummyData";
 
-const DeleteProduct = () => {
+const DeleteProduct = ({productId}:any) => {
   const [deleteProduct,{data,isError,isLoading}]=useDeleteProductMutation();
 
   if(isError){
@@ -10,8 +10,19 @@ const DeleteProduct = () => {
   if(isLoading){
     return <h1>Loading</h1>
   }
+
+  const handleDelete=async()=>{
+    try{
+      await deleteProduct(productId);
+    }catch(err){
+      console.log(err)
+    }
+  };
+
   return (
-    <div>DeleteProduct</div>
+    <div>
+      <button onClick={handleDelete} disabled={isLoading}>Delete Product</button>
+    </div>
   )
 }
 
