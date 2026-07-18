7653ef4a13c27dcc0054f11af7212cd214060619
Hrishi <rathodhrishi12345@gmail.com>
2026-07-18 22:41:54 +0530

    Adding border to the buttons

--- dp-challenges/src/components/Header.tsx
+++ dp-challenges/src/components/Header.tsx
@@ -4,10 +4,10 @@ const Header = () => {
     <header className="bg-teal-600 flex justify-between items-center p-4 text-white">
       <h1 className="text-xl font-bold">My Dashboard</h1>
       <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2 cursor-pointer">
+        <button className="bg-blue-500 hover:bg-blue-700 border border-gray-600 text-white font-bold py-2 px-4 rounded m-2 cursor-pointer">
           Profile
         </button>
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded m-2 cursor-pointer">
+        <button className="bg-red-500 hover:bg-red-700 border border-gray-600 text-white font-bold py-2 px-4 rounded m-2 cursor-pointer">
           Logout
         </button>
       </div>
